886db9a81ad83f6503ab402d80756653eb43580e
allstargaurav <allstargaurav@gmail.com>
2020-11-23 01:16:04 +0100

    OmniJaws: add vectorized Weather client icons
    Change-Id: I146141077e3b520c7ac7e1e6c62c6480ab622376
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
+++ app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
@@ -64,7 +64,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
         OmniJawsClient.OmniJawsObserver, OnMainSwitchChangeListener {
 
     private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
-    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws.google";
+    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws.weatherclient";
 
     private SharedPreferences mPrefs;
     private ListPreference mProvider;
