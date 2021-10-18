6db740a5eb39b2c29e120da4d7a0b75145939206
micky387 <mickaelsaibi@free.fr>
2021-10-18 10:28:12 +0200

    OmniJaws: Fix required libraries between the build system and the manifest
    https://source.android.com/devices/tech/dalvik/art-class-loader-context
    https://github.com/aosp-mirror/platform_build/blob/master/Changes.md
    
    Change-Id: Id8cf2ec5eb595d295142dd135ceb21ea891dd1d1

--- Android.mk
+++ Android.mk
@@ -18,6 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_JAVA_LIBRARIES := org.apache.http.legacy
+LOCAL_OPTIONAL_USES_LIBRARIES := org.apache.http.legacy
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_USE_AAPT2 := true
