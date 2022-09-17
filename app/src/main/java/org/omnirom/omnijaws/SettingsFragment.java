58cf0a148b27fbf93bdc418308825c3bf4e1650d
Pranav Vashi <neobuddy89@gmail.com>
2022-09-18 01:37:30 +0530

    OmniJaws: Set default icon package to google
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
+++ app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
@@ -61,7 +61,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
         OmniJawsClient.OmniJawsObserver {
 
     private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
-    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws.outline";
+    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws.google";
 
     private SharedPreferences mPrefs;
     private ListPreference mProvider;
