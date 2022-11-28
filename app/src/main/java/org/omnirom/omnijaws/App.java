646d75044c9bd98f996e930bb15f0994e98acdae
maxwen <max.weninger@gmail.com>
2022-11-28 20:47:51 +0100

    OmniJaws: add dynamic colors support
    Change-Id: Icecc964f6f681b692bdec5383d2645821c722359

---
+++ app/src/main/java/org/omnirom/omnijaws/App.java
@@ -0,0 +1,15 @@
+package org.omnirom.omnijaws;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(
+                this, R.style.AppTheme_Overlay);
+    }
+}
